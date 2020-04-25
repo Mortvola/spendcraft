ffed85c878b0df34925d71eb2ce2700ab30e411f
richard <richard@sorenson.com>
2020-04-24 20:52:41 -0600

    Added @production tag to select between production react and development react.

--- start/hooks.js
+++ start/hooks.js
@@ -125,6 +125,75 @@ hooks.after.providersBooted(() => {
         }
     });
 
+  class ProductionTag extends View.engine.BaseTag {
+    /**
+     * The tag name
+     *
+     * @method tagName
+     *
+     * @return {String}
+     */
+    get tagName () {
+      return 'production'
+    }
+
+    /**
+     * Tag is not a block tag
+     *
+     * @method isBlock
+     *
+     * @return {Boolean}
+     */
+    get isBlock () {
+      return true
+    }
+
+    /**
+     * Compile method to create a if block to compile
+     * content when environment is production
+     *
+     * @method compile
+     *
+     * @param  {Object} compiler
+     * @param  {Object} lexer
+     * @param  {Object} buffer
+     * @param  {String} options.body
+     * @param  {Array} options.childs
+     * @param  {Number} options.lineno
+     *
+     * @return {void}
+     */
+    compile (compiler, lexer, buffer, { body, childs, lineno }) {
+		let production = Env.get("NODE_ENV") == "production";
+
+      /**
+       * Open if tag
+       */
+      buffer.writeLine(`if (${production}) {`)
+      buffer.indent()
+
+      /**
+       * Re-parse all childs via compiler.
+       */
+      childs.forEach((child) => compiler.parseLine(child))
+
+      /**
+       * Close the if tag
+       */
+      buffer.dedent()
+      buffer.writeLine('}')
+    }
+
+    /**
+     * Nothing needs to be done at runtime
+     *
+     * @method run
+     */
+    run () {}
+  }
+
+  View.tag(new ProductionTag())
+
   class ErrorTag extends View.engine.BaseTag {
     //
       get tagName () {
