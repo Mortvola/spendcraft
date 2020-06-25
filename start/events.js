2a5dd8832c1d497f10bc52b52e0838eba6dc77fb
richard <richard@sorenson.com>
2020-06-25 04:29:28 -0600

    Fixed email verification URL.

--- start/events.js
+++ start/events.js
@@ -1,19 +1,16 @@
-'use strict'
-
 const querystring = require('querystring');
 
 const Event = use('Event')
 const Mail = use('Mail')
 const Env = use('Env')
 
-Event.on('user::created', async ({user, token}) => {
+Event.on('user::created', async ({ user, token }) => {
+    const verificationLink = `${Env.get('APP_URL')}/activate?${querystring.encode({ token })}`;
 
-    let verificationLink = "http://192.168.1.28:3333/activate?" + querystring.encode({token: token});
-
-    await Mail.send('emails.welcome', {verificationLink}, (message) => {
+    await Mail.send('emails.welcome', { verificationLink }, (message) => {
         message
-          .to(user.email)
-          .from(Env.get('MAIL_FROM_ADDRESS'))
-          .subject('Verify Email Address');
+            .to(user.email)
+            .from(Env.get('MAIL_FROM_ADDRESS'))
+            .subject('Verify Email Address');
     });
 });
