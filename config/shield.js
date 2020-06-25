fd2667a074278c77a78b3b82c69267ef7e0f0e89
Richard Shields <richard@debertas.com>
2020-06-25 14:01:17 +0000

    Added /wh to shield filter list

--- config/shield.js
+++ config/shield.js
@@ -134,7 +134,7 @@ module.exports = {
   csrf: {
     enable: true,
     methods: ['POST', 'PUT', 'DELETE'],
-    filterUris: [],
+    filterUris: ['/wh'],
     cookieOptions: {
       httpOnly: true,
       sameSite: true,
