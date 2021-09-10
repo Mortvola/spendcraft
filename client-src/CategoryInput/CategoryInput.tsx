35e253773e1b57e8abdf840e9a7b743e6b97f3b6
Mortvola <mortvola@gmail.com>
2021-09-10 14:28:05 -0600

    Improved user experience with category selector.

--- client-src/CategoryInput/CategoryInput.tsx
+++ client-src/CategoryInput/CategoryInput.tsx
@@ -5,9 +5,10 @@ import ReactDOM from 'react-dom';
 import CategorySelector, { categoryFiltered } from './CategorySelector';
 import useExclusiveBool from '../ExclusiveBool';
 import MobxStore from '../state/mobxStore';
-import { CategoryInterface, GroupInterface } from '../state/State';
+import { CategoryInterface } from '../state/State';
 import { isGroup } from '../state/Group';
 import { isCategory } from '../state/Category';
+import { TreeNodeInterface } from '../state/CategoryTree';
 
 type PropsType = {
   categoryId: number | null,
@@ -26,18 +27,18 @@ const CategoryInput = ({
     { groupIndex: null, categoryIndex: null },
   );
 
+  const initialValue = (): string => (
+    categoryId === null || (categoryTree.unassignedCat && categoryId === categoryTree.unassignedCat.id)
+      ? ''
+      : (categoryTree.getCategoryName(categoryId) ?? '')
+  );
+
   const [open, setOpen] = useExclusiveBool(false);
-  const [value, setValue] = useState<string | null>(
-    categoryId === null || (categoryTree.unassignedCat && categoryId === categoryTree.unassignedCat.id)
-      ? null
-      : categoryTree.getCategoryName(categoryId),
-  );
-  const [originalValue, setOriginalValue] = useState<string | null>(
-    categoryId === null || (categoryTree.unassignedCat && categoryId === categoryTree.unassignedCat.id)
-      ? null
-      : categoryTree.getCategoryName(categoryId),
-  );
-  const [filter, setFilter] = useState<{ value: string, parts: string[]}>({ value: '', parts: [] });
+  const [value, setValue] = useState<string>(initialValue);
+  const [originalValue, setOriginalValue] = useState<string>(initialValue);
+  const [filter, setFilter] = useState<{ value: string, parts: string[]}>({
+    value: initialValue(), parts: initialValue().toLowerCase().split(':'),
+  });
   const inputRef = useRef<HTMLInputElement | null>(null);
   const selectorRef = useRef<HTMLDivElement | null>(null);
 
@@ -45,6 +46,221 @@ const CategoryInput = ({
     setOpen(false);
     setValue(originalValue);
     setFilter({ value: '', parts: [] });
+  };
+
+  const handleSelect = (category: CategoryInterface) => {
+    let groupIndex: number | null = nodes.findIndex((g) => g.id === category.id || g.id === category.groupId);
+    let categoryIndex: number;
+
+    const group = nodes[groupIndex];
+    if (isGroup(group)) {
+      categoryIndex = group.categories.findIndex((c) => c.id === category.id);
+    }
+    else {
+      categoryIndex = groupIndex;
+      groupIndex = null;
+    }
+
+    setSelected({ groupIndex, categoryIndex });
+    setOpen(false);
+    setValue(categoryTree.getCategoryName(category.id) ?? '');
+    setOriginalValue(categoryId === null ? '' : (categoryTree.getCategoryName(categoryId) ?? ''));
+
+    if (onChange) {
+      onChange(category);
+    }
+  };
+
+  const filtered = (groupIndex: number, categoryIndex: number, filterParts: string[]): boolean => {
+    const node = nodes[groupIndex];
+    return (isGroup(node) && (
+      node.categories.length === 0
+      || categoryFiltered(node, node.categories[categoryIndex], filterParts)
+    )) || (isCategory(node) && categoryFiltered(null, node, filterParts));
+  };
+
+  const traverseList = (
+    selection: Selection,
+    moveOne: (selection: Selection) => boolean,
+    filterParts: string[],
+  ): Selection | null => {
+    const newSelection: Selection = { ...selection };
+
+    for (;;) {
+      if (moveOne(newSelection)) {
+        if (newSelection.groupIndex === null) {
+          throw new Error('group index is null');
+        }
+
+        if (newSelection.categoryIndex === null) {
+          throw new Error('category index is null');
+        }
+
+        if (!filtered(newSelection.groupIndex, newSelection.categoryIndex, filterParts)) {
+          return newSelection;
+        }
+      }
+      else {
+        return null;
+      }
+    }
+  }
+
+  const moveDownOne = (newSelection: Selection): boolean => {
+    if (newSelection.groupIndex === null || newSelection.categoryIndex === null) {
+      newSelection.groupIndex = 0;
+      newSelection.categoryIndex = 0;
+    }
+    else {
+      let node = nodes[newSelection.groupIndex];
+      if (isCategory(node)) {
+        if (newSelection.groupIndex < nodes.length - 1) {
+          newSelection.groupIndex += 1;
+          node = nodes[newSelection.groupIndex];
+          newSelection.categoryIndex = 0;
+        }
+        else {
+          return false;
+        }
+      }
+      else if (newSelection.categoryIndex < node.categories.length - 1) {
+        newSelection.categoryIndex += 1;
+      }
+      else if (newSelection.groupIndex < nodes.length - 1) {
+        newSelection.groupIndex += 1;
+        node = nodes[newSelection.groupIndex];
+        newSelection.categoryIndex = 0;
+      }
+      else {
+        // Could not move down
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  const setFirstBestSelection = (filterParts: string[]) => {
+    const selection = traverseList({ groupIndex: null, categoryIndex: null }, moveDownOne, filterParts);
+    if (selection === null) {
+      setSelected({ groupIndex: null, categoryIndex: null })
+    }
+    else {
+      setSelected(selection);
+    }
+  };
+
+  const openDropDown = () => {
+    const origValue = categoryId === null ? '' : (categoryTree.getCategoryName(categoryId) ?? '');
+    setOriginalValue(origValue);
+    setFilter({ value, parts: value.toLowerCase().split(':') });
+    setOpen(true);
+    setFirstBestSelection(filter.parts);
+  };
+
+  const handleClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    if (!open) {
+      openDropDown();
+    }
+  };
+
+  const handleFocus = () => {
+    openDropDown();
+  }
+
+  const handleBlur = () => {
+    setOpen(false);
+  };
+
+  const moveUpOne = (newSelection: Selection): boolean => {
+    if (newSelection.groupIndex === null || newSelection.categoryIndex === null) {
+      newSelection.groupIndex = 0;
+      newSelection.categoryIndex = 0;
+    }
+    else {
+      let node = nodes[newSelection.groupIndex];
+      if (isCategory(node)) {
+        if (newSelection.groupIndex > 0) {
+          newSelection.groupIndex -= 1;
+          node = nodes[newSelection.groupIndex];
+          newSelection.categoryIndex = 0;
+          if (isGroup(node) && node.categories.length > 0) {
+            newSelection.categoryIndex = node.categories.length - 1;
+          }
+        }
+        else {
+          return false;
+        }
+      }
+      else if (newSelection.categoryIndex > 0) {
+        newSelection.categoryIndex -= 1;
+      }
+      else if (newSelection.groupIndex > 0) {
+        newSelection.groupIndex -= 1;
+        node = nodes[newSelection.groupIndex];
+        newSelection.categoryIndex = 0;
+        if (isGroup(node) && node.categories.length > 0) {
+          newSelection.categoryIndex = node.categories.length - 1;
+        }
+      }
+      else {
+        // Could not move up
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  const handleDown = (filterParts: string[]) => {
+    const selection = traverseList(selected, moveDownOne, filterParts);
+    if (selection !== null) {
+      setSelected(selection);
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+    const newFilter = {
+      value: event.target.value,
+      parts: event.target.value !== '' ? event.target.value.toLowerCase().split(':') : [],
+    };
+    setFilter(newFilter);
+    setFirstBestSelection(newFilter.parts);
+  };
+
+  const handleUp = (filterParts: string[]) => {
+    const selection = traverseList(selected, moveUpOne, filterParts);
+    if (selection !== null) {
+      setSelected(selection);
+    }
+  };
+
+  const handleEnter = () => {
+    let selectedGroup: TreeNodeInterface | null = null;
+    if (selected.groupIndex !== null) {
+      selectedGroup = nodes[selected.groupIndex];
+
+      if (isCategory(selectedGroup)) {
+        setValue(categoryTree.getCategoryName(selectedGroup.id) ?? '');
+        if (onChange) {
+          onChange(selectedGroup);
+        }
+      }
+      else if (isGroup(selectedGroup) && selectedGroup && selected.categoryIndex !== null) {
+        const selectedCategory = selectedGroup.categories[selected.categoryIndex];
+        setValue(categoryTree.getCategoryName(selectedCategory.id) ?? '');
+        if (onChange) {
+          onChange(selectedCategory);
+        }
+      }
+      else {
+        setValue('');
+      }
+    }
+
+    setOpen(false);
   };
 
   const handleMouseDown = (event: MouseEvent) => {
@@ -69,199 +285,6 @@ const CategoryInput = ({
     return undefined;
   });
 
-  const openDropDown = () => {
-    setOriginalValue(categoryId === null ? null : categoryTree.getCategoryName(categoryId));
-    setOpen(true);
-  };
-
-  const handleClick = (event: React.MouseEvent) => {
-    event.stopPropagation();
-    if (!open) {
-      openDropDown();
-    }
-  };
-
-  const handleSelect = (category: CategoryInterface) => {
-    let groupIndex: number | null = nodes.findIndex((g) => g.id === category.id || g.id === category.groupId);
-    let categoryIndex: number;
-
-    const group = nodes[groupIndex];
-    if (isGroup(group)) {
-      categoryIndex = group.categories.findIndex((c) => c.id === category.id);
-    }
-    else {
-      categoryIndex = groupIndex;
-      groupIndex = null;
-    }
-
-    setSelected({ groupIndex, categoryIndex });
-    setOpen(false);
-    setValue(categoryTree.getCategoryName(category.id));
-    setOriginalValue(categoryId === null ? null : categoryTree.getCategoryName(categoryId));
-
-    if (onChange) {
-      onChange(category);
-    }
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-    setFilter({
-      value: event.target.value,
-      parts: event.target.value !== '' ? event.target.value.toLowerCase().split(':') : [],
-    });
-  };
-
-  const handleBlur = () => {
-    setOpen(false);
-  };
-
-  const filtered = (groupIndex: number, categoryIndex: number): boolean => {
-    const node = nodes[groupIndex];
-    return (isGroup(node) && (
-      node.categories.length === 0
-      || categoryFiltered(node, node.categories[categoryIndex], filter.parts)
-    )) || (isCategory(node) && categoryFiltered(null, node, filter.parts));
-  };
-
-  const traverseList = (moveOne: (selection: Selection) => boolean): Selection | null => {
-    const newSelection: Selection = {
-      groupIndex: selected.groupIndex,
-      categoryIndex: selected.categoryIndex,
-    };
-
-    for (;;) {
-      if (moveOne(newSelection)) {
-        if (newSelection.groupIndex === null) {
-          throw new Error('group index is null');
-        }
-
-        if (newSelection.categoryIndex === null) {
-          throw new Error('category index is null');
-        }
-
-        if (!filtered(newSelection.groupIndex, newSelection.categoryIndex)) {
-          return newSelection;
-        }
-      }
-      else {
-        return null;
-      }
-    }
-  }
-
-  const handleDown = () => {
-    const moveDownOne = (newSelection: Selection): boolean => {
-      if (newSelection.groupIndex === null || newSelection.categoryIndex === null) {
-        newSelection.groupIndex = 0;
-        newSelection.categoryIndex = 0;
-      }
-      else {
-        let node = nodes[newSelection.groupIndex];
-        if (isCategory(node)) {
-          if (newSelection.groupIndex < nodes.length - 1) {
-            newSelection.groupIndex += 1;
-            node = nodes[newSelection.groupIndex];
-            newSelection.categoryIndex = 0;
-          }
-          else {
-            return false;
-          }
-        }
-        else if (newSelection.categoryIndex < node.categories.length - 1) {
-          newSelection.categoryIndex += 1;
-        }
-        else if (newSelection.groupIndex < nodes.length - 1) {
-          newSelection.groupIndex += 1;
-          node = nodes[newSelection.groupIndex];
-          newSelection.categoryIndex = 0;
-        }
-        else {
-          // Could not move down
-          return false;
-        }
-      }
-
-      return true;
-    }
-
-    const selection = traverseList(moveDownOne);
-    if (selection !== null) {
-      setSelected(selection);
-    }
-  };
-
-  const handleUp = () => {
-    const moveUpOne = (newSelection: Selection): boolean => {
-      if (newSelection.groupIndex === null || newSelection.categoryIndex === null) {
-        newSelection.groupIndex = 0;
-        newSelection.categoryIndex = 0;
-      }
-      else {
-        let node = nodes[newSelection.groupIndex];
-        if (isCategory(node)) {
-          if (newSelection.groupIndex > 0) {
-            newSelection.groupIndex -= 1;
-            node = nodes[newSelection.groupIndex];
-            newSelection.categoryIndex = 0;
-            if (isGroup(node) && node.categories.length > 0) {
-              newSelection.categoryIndex = node.categories.length - 1;
-            }
-          }
-          else {
-            return false;
-          }
-        }
-        else if (newSelection.categoryIndex > 0) {
-          newSelection.categoryIndex -= 1;
-        }
-        else if (newSelection.groupIndex > 0) {
-          newSelection.groupIndex -= 1;
-          node = nodes[newSelection.groupIndex];
-          newSelection.categoryIndex = 0;
-          if (isGroup(node) && node.categories.length > 0) {
-            newSelection.categoryIndex = node.categories.length - 1;
-          }
-        }
-        else {
-          // Could not move up
-          return false;
-        }
-      }
-
-      return true;
-    }
-
-    const selection = traverseList(moveUpOne);
-    if (selection !== null) {
-      setSelected(selection);
-    }
-  };
-
-  const handleEnter = () => {
-    if (selected.categoryIndex !== null) {
-      let selectedGroup = null;
-      if (selected.groupIndex !== null) {
-        selectedGroup = nodes[selected.groupIndex];
-      }
-      let selectedCategory = null;
-      if (selectedGroup && isGroup(selectedGroup)) {
-        selectedCategory = selectedGroup.categories[selected.categoryIndex];
-      }
-
-      setOpen(false);
-      if (selectedCategory === null) {
-        setValue(null);
-      }
-      else {
-        setValue(categoryTree.getCategoryName(selectedCategory.id));
-        if (onChange) {
-          onChange(selectedCategory);
-        }
-      }
-    }
-  };
-
   const handleKeydown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (open && selectorRef.current) {
       switch (event.key) {
@@ -272,12 +295,17 @@ const CategoryInput = ({
           break;
         }
         case 'ArrowDown':
-          handleDown();
+          event.preventDefault();
+          handleDown(filter.parts);
           break;
         case 'ArrowUp':
-          handleUp();
+          event.preventDefault();
+          handleUp(filter.parts);
           break;
         case 'Enter':
+          event.preventDefault();
+          handleEnter();
+          break;
         case 'Tab':
           handleEnter();
           break;
@@ -285,7 +313,12 @@ const CategoryInput = ({
       }
     }
     else if (event.key.length === 1 || event.key === 'Backspace' || event.key === 'Delete'
-      || event.key === 'ArrowDown') {
+      || event.key === 'ArrowDown'
+    ) {
+      if (event.key === 'ArrowDown') {
+        event.preventDefault();
+      }
+
       openDropDown();
     }
   };
@@ -308,7 +341,7 @@ const CategoryInput = ({
 
       let selectedCategory: CategoryInterface | null = null;
       if (selected.groupIndex !== null) {
-        const selectedGroup: GroupInterface | CategoryInterface = nodes[selected.groupIndex];
+        const selectedGroup: TreeNodeInterface = nodes[selected.groupIndex];
         if (isGroup(selectedGroup)) {
           if (selected.categoryIndex === null) {
             throw new Error('category index is null');
@@ -347,7 +380,7 @@ const CategoryInput = ({
     }
 
     return null;
-  };
+  }
 
   return (
     <>
@@ -358,6 +391,7 @@ const CategoryInput = ({
         placeholder="Unassigned"
         onClick={handleClick}
         onChange={handleChange}
+        onFocus={handleFocus}
         onBlur={handleBlur}
         onKeyDown={handleKeydown}
         value={value || ''}
