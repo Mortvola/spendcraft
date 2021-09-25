c6007ecf1d37def938ff9640e84ffae8baaecccb
Mortvola <mortvola@gmail.com>
2021-09-25 07:14:32 -0600

    Made landing page more responsive.

--- client-src/Intro.tsx
+++ client-src/Intro.tsx
@@ -1,10 +1,14 @@
 import React, { ReactElement } from 'react';
 import { useHistory } from 'react-router-dom';
-import 'regenerator-runtime/runtime';
+import * as responsive from 'react-responsive';
+import 'regenerator-runtime';
 import styles from './Intro.module.css';
 
 const Intro = (): ReactElement => {
   const history = useHistory();
+  const tiny = responsive.useMediaQuery({ query: '(max-width: 350px)' });
+  const small = responsive.useMediaQuery({ query: '(max-width: 600px)' });
+  const medium = responsive.useMediaQuery({ query: '(max-width: 1224px)' });
 
   const handleSignupClick = () => {
     history.push('/signup');
@@ -14,100 +18,53 @@ const Intro = (): ReactElement => {
     history.push('/signin');
   }
 
-  const className = 'flex-center position-ref full-height';
+  const addSizeClass = (className: string): string => {
+    if (tiny) {
+      return `${styles.tiny} ${className}`;
+    }
+
+    if (small) {
+      return `${styles.small} ${className}`;
+    }
+
+    if (medium) {
+      return `${styles.medium} ${className}`
+    }
+
+    return className
+  }
 
   return (
-    <div style={{ maxWidth: '1400px', justifySelf: 'center', backgroundColor: 'whitesmoke' }}>
-      <div
-        className={className}
-        style={{
-          display: 'grid',
-          gridTemplateColumns: 'minmax(0,1fr)',
-          gridTemplateRows: 'max-content minmax(0,1fr)',
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        <div
-          style={{
-            display: 'grid',
-            gridTemplateColumns: 'max-content max-content',
-            gridTemplateRows: 'max-content',
-            columnGap: '2rem',
-            marginTop: '1rem',
-            justifyContent: 'center',
-          }}
-        >
-          <div
-            className={styles.linkButton}
-            style={{ fontSize: '1.25rem' }}
-            onClick={handleSignupClick}
-          >
-            Sign up
-          </div>
-          <div
-            className={styles.linkButton}
-            style={{ fontSize: '1.25rem' }}
-            onClick={handleSigninClick}
-          >
-            Sign in
-          </div>
+    <div className={addSizeClass(`${styles.frame} flex-center position-ref full-height`)}>
+      <div className={styles.buttonTray}>
+        <div className={styles.linkButton} onClick={handleSignupClick}>
+          Sign up
         </div>
-        <div
-          style={{
-            display: 'grid',
-            gridTemplateColumns: 'minmax(0,1.2fr) minmax(0,2fr)',
-            gridTemplateRows: 'minmax(0,1fr)',
-            width: '100%',
-            height: '100%',
-            alignItems: 'start',
-            justifyItems: 'center',
-          }}
-        >
-          <div
-            style={{
-              marginLeft: '2rem', display: 'flex', flexDirection: 'column', alignItems: 'start',
-            }}
-          >
-            <div
-              style={{
-                fontSize: '4.5rem',
-                fontFamily: 'Georgiate, sans-serif',
-                color: '#8e2727',
-              }}
-            >
-              SpendCraft
-            </div>
-            <div style={{ fontSize: '2rem' }}>We all spend money.</div>
-            <div style={{ fontSize: '2rem' }}>Be smart about how you spend it.</div>
-            <div style={{ marginTop: '1rem', marginBottom: '2rem' }}>
-              With an easy to use envelope style budgeting system,
-              take control of your expenditures and spend your money
-              on the things that are important to you.
-            </div>
-            <a
-              href="/signup"
-              className={styles.linkButton}
-              style={{
-                fontSize: '2rem',
-                padding: '0.5rem 1rem',
-                backgroundColor: '#308730',
-                color: 'white',
-              }}
-            >
-              Sign up for free
-            </a>
-          </div>
-          <img
-            src="/budgeting.svg"
-            alt=""
-            style={{
-              maxWidth: '90%',
-              maxHeight: '90%',
-              alignSelf: 'start',
-            }}
-          />
+        <div className={styles.linkButton} onClick={handleSigninClick}>
+          Sign in
         </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.introText}>
+          <div className={styles.title}>
+            SpendCraft
+          </div>
+          <div className={styles.catchPhrase}>We all spend money.</div>
+          <div className={styles.catchPhrase}>Be smart about how you spend it.</div>
+          <div className={styles.subCatchPhrase}>
+            With an easy to use envelope style budgeting system,
+            take control of your expenditures and spend your money
+            on the things that are important to you.
+          </div>
+          <a href="/signup" className={`${styles.linkButton} ${styles.signupFreeButton}`}>
+            Sign up for free
+          </a>
+        </div>
+        <img
+          src="/budgeting.svg"
+          alt=""
+          className={styles.image}
+        />
       </div>
     </div>
   );
