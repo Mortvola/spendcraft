96b7e734d4185c7e0fced0cf4ef181acfeb4dea0
mortvola <shields12345@msn.com>
2022-10-04 09:10:12 -0600

    Added code to remove bad device tokens. Changed generation of provider jwt so that it regenerates if the token is more than 30 minutes old.

--- providers/ApplePushNotifications/index.ts
+++ providers/ApplePushNotifications/index.ts
@@ -5,14 +5,17 @@ import { importPKCS8, KeyLike } from 'jose/key/import';
 import { DateTime } from "luxon";
 import Application from 'App/Models/Application';
 import Logger from '@ioc:Adonis/Core/Logger';
+import ApnsToken from 'App/Models/ApnsToken';
 
 class ApplePushNotifications {
-  pushNotificationKey: KeyLike | null = null
+  pushNotificationKey: KeyLike | null = null;
+  
+  providerJwtTime: DateTime | null;
+
+  providerJwt: string | null = null;
 
   public async sendPushNotifications(application: Application) {
     try {
-      await this.loadPushNotificationKey();
-
       const users = await application.related('users').query();
       // const users = await User.query().where('applicationId', application.id)
 
@@ -22,7 +25,7 @@ class ApplePushNotifications {
 
           await Promise.all(userTokens.map(async (userToken) => {
             try {
-              await this.sendPushNotification(userToken.token);
+              await this.sendPushNotification(userToken);
             }
             catch (error) {
               Logger.error({ err: error }, 'Push notification failed');
@@ -36,31 +39,41 @@ class ApplePushNotifications {
     }
   }
 
-  private async loadPushNotificationKey() {
+  private async generateProviderJWT() {
     if (this.pushNotificationKey === null) {
       const privateKey = fs.readFileSync('./PushNotificationKey.p8')
       this.pushNotificationKey = await importPKCS8(privateKey.toString(), 'ES256');
-
-      if (this.pushNotificationKey === null) {
-        throw new Error('push notification key is null');
-      }
     }
+
+    if (this.pushNotificationKey === null) {
+      throw new Error('push notification key is null');
+    }
+
+    if (this.providerJwt === null || this.providerJwtTime == null
+      || this.providerJwtTime.diff(DateTime.now()).as('minutes') > 30
+    ) {
+      this.providerJwtTime = DateTime.now();
+  
+      this.providerJwt = await new SignJWT({
+        iss: 'N7MR48SV68', // Team ID
+        iat: this.providerJwtTime.toSeconds(),
+      })
+        .setProtectedHeader({
+          alg: 'ES256',
+          kid: 'L5KGW6G49S', // push notification key ID
+        })
+        .sign(this.pushNotificationKey)
+      }
   }
 
-  private async sendPushNotification(deviceToken: string) {
-    if (this.pushNotificationKey === null) {
-      throw new Error('push notificateion key is null')
-    }
+  private async sendPushNotification(deviceToken: ApnsToken) {
+    await this.generateProviderJWT();
 
-    const jwt = await new SignJWT({
-      iss: 'N7MR48SV68', // Team ID
-      iat: DateTime.now().toSeconds(),
-    })
-      .setProtectedHeader({
-        alg: 'ES256',
-        kid: 'L5KGW6G49S', // push notification key ID
-      })
-      .sign(this.pushNotificationKey)
+    const jwt = this.providerJwt;
+
+    if (jwt === null) {
+      throw new Error('provider jwt is null')
+    }
 
     const notification = {
       aps: {
@@ -73,9 +86,9 @@ class ApplePushNotifications {
     }
 
     try {
-      Logger.info(`pushing notification to ${deviceToken}`);
+      Logger.info(`pushing notification to ${deviceToken.token}`);
 
-      const response = await fetch(`https://api.push.apple.com/3/device/${deviceToken}`, {
+      const response = await fetch(`https://api.push.apple.com/3/device/${deviceToken.token}`, {
         method: 'POST',
         body: JSON.stringify(notification),
         headers: {
@@ -89,10 +102,16 @@ class ApplePushNotifications {
       })
 
       if (response.ok) {
-        Logger.info(`apns success, device token: ${deviceToken}, apns id: ${response.headers.get('apns-id')}`);
+        Logger.info(`apns success, device token: ${deviceToken.token}, apns id: ${response.headers.get('apns-id')}`);
       }
       else {
         const body = await response.json();
+
+        if (response.status === 400 && body.reason === 'BadDeviceToken') {
+          // Remove the bad device token
+          await deviceToken.delete();
+        }
+
         Logger.error(`apns failure: ${response.status}: ${response.statusText}, body: ${JSON.stringify(body)}`)
       }
     }
