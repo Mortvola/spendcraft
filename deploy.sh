7dcabb8b01676f33ebbdc36d1f2d85043f2aa63f
mortvola <shields12345@msn.com>
2023-01-01 10:19:24 -0700

    Updaed packages.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ set -e
 
 rm build.zip;
 zip -r build build;
-# to-debertas build.zip
 
-scp -i ~/.ssh/debertas.pem build.zip ubuntu@ec2-54-190-121-102.us-west-2.compute.amazonaws.com:
+scp -i ~/.ssh/debertas.pem build.zip ubuntu@${SPENDCRAFT_SERVER}:
+
