1ac76f4c1f7f4e00fec6b4cfce489c188bacb150
Mortvola <mortvola@gmail.com>
2023-10-09 07:14:35 -0600

    Added missing database migration to move sync_date from account to institution.

---
+++ database/migrations/1696776068618_institutions.ts
@@ -0,0 +1,19 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'institutions'
+
+  public async up () {
+    this.schema.alterTable(this.tableName, (table) => {
+      // alter table
+      table.timestamp('sync_date');
+    })
+  }
+
+  public async down () {
+    this.schema.alterTable(this.tableName, (table) => {
+      // reverse alternations
+      table.dropColumn('sync_date');
+    })
+  }
+}
