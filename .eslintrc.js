2812880dc95db62fabf38871a8e11d33af60822a
Mortvola <mortvola@gmail.com>
2023-10-23 09:05:05 -0600

    Fixed remaining issues with eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json", "./client-src/tsconfig.json"],
+    project: ['./tsconfig.json', './client-src/tsconfig.json'],
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
@@ -12,13 +12,13 @@ module.exports = {
   plugins: [
     'react',
     '@typescript-eslint',
-    'css-modules'
+    'css-modules',
   ],
   extends: [
     'airbnb',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    "plugin:adonis/typescriptApp",
+    'plugin:adonis/typescriptApp',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:import/recommended',
@@ -26,54 +26,54 @@ module.exports = {
     'plugin:css-modules/recommended',
   ],
   rules: {
-    "react/require-default-props": "off",
-    "react/jsx-props-no-spreading": ["off"],
-    "react/jsx-filename-extension": [1, { "extensions": [".tsx", ".jsx"] }],
-    "react/function-component-definition": [2, { "namedComponents": "arrow-function" }],
-    "no-shadow": "off",
-    "@typescript-eslint/no-shadow": ["error"],
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": ["error"],
-    "brace-style": ["error", "stroustrup"],
-    "jsx-a11y/click-events-have-key-events": ["off"],
-    "jsx-a11y/no-static-element-interactions": ["off"],
-    "no-param-reassign": ["error", { "props": false }],
-    "jsx-a11y/label-has-associated-control": ["off"],
-    "semi": "off",
-    "import/no-unresolved": ["off"],
-    "@typescript-eslint/semi": [
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': ['off'],
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'brace-style': ['error', 'stroustrup'],
+    'jsx-a11y/click-events-have-key-events': ['off'],
+    'jsx-a11y/no-static-element-interactions': ['off'],
+    'no-param-reassign': ['error', { props: false }],
+    'jsx-a11y/label-has-associated-control': ['off'],
+    semi: 'off',
+    'import/no-unresolved': ['off'],
+    '@typescript-eslint/semi': [
       0,
-      "always"
+      'always',
     ],
-    "space-before-function-paren": [
-      "off"
+    'space-before-function-paren': [
+      'off',
     ],
-    "@typescript-eslint/space-before-function-paren": [
-      "off",
+    '@typescript-eslint/space-before-function-paren': [
+      'off',
     ],
-    "@typescript-eslint/explicit-member-accessibility": "off",
+    '@typescript-eslint/explicit-member-accessibility': 'off',
     'import/extensions': [
-      "error",
-      "ignorePackages",
+      'error',
+      'ignorePackages',
       {
-        "js": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      }
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
     ],
-    "css-modules/no-unused-class": "off",
-    "css-modules/no-undef-class": [2, { "camelCase": true }],
-    "function-paren-newline": ["error", "consistent"],
+    'css-modules/no-unused-class': 'off',
+    'css-modules/no-undef-class': [2, { camelCase: true }],
+    'function-paren-newline': ['error', 'consistent'],
   },
   settings: {
     'import/resolver': {
       typescript: {
         project: [
-          "./tsconfig.json",
-          "./client-src/tsconfig.json"
+          './tsconfig.json',
+          './client-src/tsconfig.json',
         ],
       },
-    }
-  }
+    },
+  },
 };
