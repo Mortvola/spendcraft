6abf27b1e4034447ef37c00b0dd19195ca7422cf
Mortvola <mortvola@gmail.com>
2023-10-28 09:23:08 -0600

    Updated sizes of buttons to accommodate smaller devices. Fixed alignment of caption text under buttons.

--- client-src/IconButton.tsx
+++ client-src/IconButton.tsx
@@ -21,11 +21,13 @@ const IconButton: React.FC<PropsType> = ({
   solid = true,
   onClick,
 }) => (
-  <button type="button" className={`btn btn-sm ${className ?? ''}`} onClick={onClick}>
+  <button type="button" className={`btn btn-sm ${styles.layout} ${className ?? ''}`} onClick={onClick}>
     <Icon icon={icon} rotate={rotate} iconClass={iconClass} solid={solid} />
-    {
-      caption
-    }
+    <div>
+      {
+        caption
+      }
+    </div>
   </button>
 );
 
