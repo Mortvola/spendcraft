4a9ae2a95bdf99588edaf03e97fa9c664b375a06
Mortvola <mortvola@gmail.com>
2024-12-14 13:41:27 -0700

    Fixed bug where pending charges were being reconciled.

--- app/Controllers/Http/AccountsController.ts
+++ app/Controllers/Http/AccountsController.ts
@@ -531,13 +531,15 @@ export default class AccountsController {
 
     const statements = await account.related('statements').query()
       .withAggregate('accountTransactions', (query) => {
-        query.where('amount', '>', 0).sum('amount').as('credits')
+        query
+          .where('amount', '>', 0).sum('amount').as('credits')
           .whereHas('transaction', (query2) => {
             query2.where('deleted', false)
           })
       })
       .withAggregate('accountTransactions', (query) => {
-        query.where('amount', '<', 0).sum('amount').as('debits')
+        query
+          .where('amount', '<', 0).sum('amount').as('debits')
           .whereHas('transaction', (query2) => {
             query2.where('deleted', false)
           })
@@ -647,6 +649,7 @@ export default class AccountsController {
           const transactions = await AccountTransaction.query({ client: trx })
             .where('accountId', statement.accountId)
             .whereNull('statementId')
+            .where('pending', false)
             .whereHas('transaction', (query) => {
               query
                 .where('deleted', false)
