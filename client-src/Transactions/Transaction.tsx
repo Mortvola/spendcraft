0a966945b75e09ffaae03873f80846582aceeb26
Mortvola <mortvola@gmail.com>
2025-05-05 21:08:34 -0600

    Fixed display of amounts in transactions.

--- client-src/Transactions/Transaction.tsx
+++ client-src/Transactions/Transaction.tsx
@@ -41,10 +41,10 @@ const Transaction: React.FC<PropsType> = observer(({
       ].includes(transaction.type)
       || transaction.amount === amount
     ) {
-      return <div className={styles.trxAmount} />;
+      return <div className={`${styles.trxAmount} currency`} />;
     }
 
-    return <Amount className={styles.trxAmount} amount={transaction.amount} />
+    return <Amount className={`${styles.trxAmount} currency`} amount={transaction.amount} />
   }
 
   const loanFields = () => (
@@ -81,8 +81,8 @@ const Transaction: React.FC<PropsType> = observer(({
       }
       <Date className={styles.date} date={transaction.date} />
       <div className={styles.name}>{transaction.name}</div>
-      <Amount className={styles.amount} amount={amount} />
-      <Amount className={styles.runningBalance} amount={runningBalance} />
+      <Amount className={`${styles.amount} currency`} amount={amount} />
+      <Amount className={`${styles.runningBalance} currency`} amount={runningBalance} />
       <TransactionAccount transaction={transaction} />
       {transactionAmount()}
       { loanFields() }
