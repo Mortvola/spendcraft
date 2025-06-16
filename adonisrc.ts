ce28e1d3ad7aa19292fbcb6652d4c2d0f77c24aa
Mortvola <mortvola@gmail.com>
2025-06-16 11:18:22 -0600

    Upgrade RC File

---
+++ adonisrc.ts
@@ -0,0 +1,72 @@
+import { defineConfig } from "@adonisjs/core/app";
+
+export default defineConfig({
+  /*
+  |--------------------------------------------------------------------------
+  | Commands
+  |--------------------------------------------------------------------------
+  |
+  | List of ace commands to register from packages. The application commands
+  | will be scanned automatically from the "./commands" directory.
+  |
+  */
+  commands: [() => import('@adonisjs/core/commands'), () => import('@adonisjs/lucid/commands'), () => import('@adonisjs/mail/commands')],
+  /*
+  |--------------------------------------------------------------------------
+  | Preloads
+  |--------------------------------------------------------------------------
+  |
+  | List of modules to import before starting the application.
+  |
+  */
+  preloads: [
+    () => import('./start/routes.js'),
+    () => import('./start/kernel.js'),
+    {
+      file: () => import('./start/validationRules'),
+      environment: ["web"],
+    },
+    {
+      file: () => import('./start/namingStrategy'),
+      environment: ["web"],
+    },
+    {
+      file: () => import('./start/events'),
+      environment: ["web"],
+    }
+  ],
+  /*
+  |--------------------------------------------------------------------------
+  | Service providers
+  |--------------------------------------------------------------------------
+  |
+  | List of service providers to import and register when booting the
+  | application
+  |
+  */
+  providers: [
+    () => import('./providers/AppProvider.js'),
+    () => import('@adonisjs/core/providers/app_provider'),
+    () => import('@adonisjs/core/providers/hash_provider'),
+    { "file": () => import('@adonisjs/core/providers/repl_provider'), "environment": ["repl", "test"] },
+    () => import('@adonisjs/session/session_provider'),
+    () => import('@adonisjs/core/providers/edge_provider'),
+    () => import('@adonisjs/lucid/database_provider'),
+    () => import('@adonisjs/auth/auth_provider'),
+    () => import('@adonisjs/shield/shield_provider'),
+    () => import('@adonisjs/mail/mail_provider'),
+    () => import('@adonisjs/redis/redis_provider'),
+    () => import('adonis5-jwt'),
+    () => import('adonis5-bullmq')
+  ],
+  metaFiles: [
+    {
+      "pattern": "public/**",
+      "reloadServer": false
+    },
+    {
+      "pattern": "resources/views/**/*.edge",
+      "reloadServer": false
+    }
+  ]
+});
