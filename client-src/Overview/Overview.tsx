72c8ff74b768bc98ed8d449a77c14dd87be6f262
Mortvola <mortvola@gmail.com>
2025-12-02 09:33:54 -0700

    Fixed bug where bills due in the same month in the future were listed in other bills due. Changed presentation so that only "other bills due" appears when there are other bills due.

--- client-src/Overview/Overview.tsx
+++ client-src/Overview/Overview.tsx
@@ -6,8 +6,21 @@ import Bill from './Bill';
 import styles from './Overview.module.scss';
 import MinorTitle from './MinorTitle';
 
-const Overview = observer(() => {
+const Overview: React.FC = observer(() => {
   const { overview } = useStores()
+
+  const otherBills = overview.bills
+    .filter((bill) => {
+      const computedGoalDate = bill.category.computedGoalDate()
+
+      return (
+        computedGoalDate && !bill.category.suspended
+        && computedGoalDate.month === DateTime.now().month
+        && computedGoalDate.year === DateTime.now().year
+        && bill.category.recurrence !== 1
+      )
+    })
+
 
   return (
     <div className={styles.layout}>
@@ -15,29 +28,40 @@ const Overview = observer(() => {
         <MinorTitle>Monthly Bills</MinorTitle>
         {
           overview.bills
-            .filter((bill) => !bill.category.suspended && bill.category.computedGoalDate()?.month === DateTime.now().month && bill.category.recurrence === 1)
+            .filter((bill) => (
+              !bill.category.suspended
+              && bill.category.computedGoalDate()?.month === DateTime.now().month
+              && bill.category.recurrence === 1)
+            )
             .map((bill) => (
               <Bill key={bill.category.id} bill={bill} />
             ))
         }
       </div>
 
-      <div>
-        <MinorTitle>Other Bills Due This Month</MinorTitle>
-        {
-          overview.bills
-            .filter((bill) => !bill.category.suspended && bill.category.computedGoalDate()?.month === DateTime.now().month && bill.category.recurrence !== 1)
-            .map((bill) => (
-              <Bill key={bill.category.id} bill={bill} />
-            ))
-        }
-      </div>
+      {
+        otherBills.length > 0
+          ? (
+            <div>
+              <MinorTitle>Other Bills Due This Month</MinorTitle>
+              {
+                otherBills.map((bill) => (
+                  <Bill key={bill.category.id} bill={bill} />
+                ))
+              }
+            </div>
+          )
+          : null
+      }
 
       <div>
         <MinorTitle>Upcoming Bills</MinorTitle>
         {
           overview.bills
-            .filter((bill) => !bill.category.suspended && bill.category.computedGoalDate()?.month !== DateTime.now().month)
+            .filter((bill) => (
+              !bill.category.suspended
+              && bill.category.computedGoalDate()?.month !== DateTime.now().month)
+            )
             .map((bill) => (
               <Bill key={bill.category.id} bill={bill} />
             ))
